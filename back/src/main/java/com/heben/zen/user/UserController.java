12ccbaa94fbd366ac1dfb6eca34dc600d896b23e
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-04-10 23:07:35 -0500

    add: api route

--- back/src/main/java/com/heben/zen/user/UserController.java
+++ back/src/main/java/com/heben/zen/user/UserController.java
@@ -2,14 +2,16 @@ package com.heben.zen.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
 import java.util.Date;
 
 @RestController
+@RequestMapping("api/v1/user")
 public class UserController {
-    @GetMapping("/user")
-    public User getUser(){
+    @GetMapping
+    public User getUsers(){
         int[] followers = {};
         int[] following = {};
         return new User(15L,
