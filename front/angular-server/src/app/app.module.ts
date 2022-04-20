8b9074bef7454071f09199f6d5d3496bc0a6ce29
Luis <luisced02031@gmail.com>
2022-04-19 23:06:10 -0500

    firebase config

--- front/angular-server/src/app/app.module.ts
+++ front/angular-server/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { AngularFireModule } from '@angular/fire';
+
+import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,9 +16,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // AngularFireModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    FirebaseTSApp.init(environment.firebaseConfig);
+  }
+}
--- front/angular-server/src/environments/environment.ts
+++ front/angular-server/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: "AIzaSyBz3Bc9wXNTwGPgxNJxkQwiWXKa1l7mbYw",
+    authDomain: "zen-social-media.firebaseapp.com",
+    projectId: "zen-social-media",
+    storageBucket: "zen-social-media.appspot.com",
+    messagingSenderId: "743793788639",
+    appId: "1:743793788639:web:3f2b8420da169f88810b6a",
+    measurementId: "G-F6T5BZ42LB"
+  }
 };
 
 /*
