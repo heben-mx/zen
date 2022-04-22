e133d69273042f22dff9b1fd11787f42de606bbc
Luis <luisced02031@gmail.com>
2022-04-21 19:24:17 -0500

    Homepage left and main side completed

--- front/angular-server/src/app/app-routing.module.ts
+++ front/angular-server/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: HomeComponent},
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule ,RouterModule.forRoot(appRoutes),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- front/angular-server/src/app/app.module.ts
+++ front/angular-server/src/app/app.module.ts
@@ -1,29 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
 // import { AngularFireModule } from '@angular/fire';
 
-import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
-import { environment } from 'src/environments/environment';
+// import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
+// import { environment } from 'src/environments/environment';
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: HomeComponent},
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
     // AngularFireModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor() {
-    FirebaseTSApp.init(environment.firebaseConfig);
-  }
+  // constructor() {
+  // //   FirebaseTSApp.init(environment.firebaseConfig);
+  // }
 }
