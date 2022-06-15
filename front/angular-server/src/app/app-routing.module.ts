6abc74d6898e49d64ce36cd3591b6a0f13823fb0
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-06-15 01:01:53 -0500

    Update de pagina 404

--- front/angular-server/src/app/app-routing.module.ts
+++ front/angular-server/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 const appRoutes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
+  {path: 'notfound', component: NotfoundComponent},
 
 ];
 
