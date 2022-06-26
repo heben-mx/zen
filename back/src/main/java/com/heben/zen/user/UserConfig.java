2e5c5915a23eac65d26dcadb0afd60269d0336a5
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-06-25 20:38:50 -0700

    Del: garbage

--- back/src/main/java/com/heben/zen/user/UserConfig.java
+++ back/src/main/java/com/heben/zen/user/UserConfig.java
@@ -1,12 +1,10 @@
 package com.heben.zen.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Configuration
