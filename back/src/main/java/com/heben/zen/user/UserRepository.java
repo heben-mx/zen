7cf30d3967c7b7b0795954e374b447deb85144e2
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-07-17 12:40:06 -0700

    Add: User: findByUsername query

--- back/src/main/java/com/heben/zen/user/UserRepository.java
+++ back/src/main/java/com/heben/zen/user/UserRepository.java
@@ -3,12 +3,17 @@ package com.heben.zen.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     Optional<User> findUserByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.username = ?1")
+    Optional<User> findByUsername(String username);
 }
