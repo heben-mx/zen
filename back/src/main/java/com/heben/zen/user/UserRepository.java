6f062f55d9cb12c8df0b701f2fefce595cba9ba4
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-07-17 14:50:15 -0700

    Add: DAO enable user

--- back/src/main/java/com/heben/zen/user/UserRepository.java
+++ back/src/main/java/com/heben/zen/user/UserRepository.java
@@ -1,6 +1,7 @@
 package com.heben.zen.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,4 +17,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.username = ?1")
     Optional<User> findByUsername(String username);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.enabled = true WHERE  u.email = ?1")
+    int enableUser(String email);
 }
