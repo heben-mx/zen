8d3d73f08e88dc2c1959e836bb2a33744997c5f9
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-07-20 18:56:20 -0700

    Add: LogInResponse to logIn(), validateSession

--- back/src/main/java/com/heben/zen/login/LogInService.java
+++ back/src/main/java/com/heben/zen/login/LogInService.java
@@ -1,6 +1,9 @@
 package com.heben.zen.login;
 
-import com.heben.zen.user.User;
+import com.heben.zen.login.session_token.ContinueSessionRequest;
+import com.heben.zen.login.session_token.SessionToken;
+import com.heben.zen.login.session_token.SessionTokenService;
+import com.heben.zen.security.Response;
 import com.heben.zen.user.UserService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,19 +11,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.UUID;
+import java.util.Optional;
 
 @Service
 public class LogInService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final UserService userService;
+    private final SessionTokenService sessionTokenService;
 
-    LogInService(BCryptPasswordEncoder bCryptPasswordEncoder, UserService userService){
+    LogInService(BCryptPasswordEncoder bCryptPasswordEncoder, UserService userService, SessionTokenService sessionTokenService){
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.userService = userService;
+        this.sessionTokenService = sessionTokenService;
     }
     @Transactional
-    public String logIn(LogInRequest request) throws IllegalAccessException {
+    public LogInResponse logIn(LogInRequest request) throws IllegalAccessException {
         String username = request.getUsername();
         String password = request.getPassword();
         UserDetails userDetails;
@@ -37,10 +42,20 @@ public class LogInService {
             throw  new IllegalAccessException("Account is expired.");
         } else {
             if (bCryptPasswordEncoder.matches(password, userDetails.getPassword())){
-                return UUID.randomUUID().toString();
+
+                return new LogInResponse((short) 200,"OK", false , sessionTokenService.generateNewToken(username));
             } else {
                 throw new IllegalAccessException("Wrong password.");
             }
         }
+    }
+
+    public Response validateSession(ContinueSessionRequest request){
+        String token = request.getToken(), username = request.getUsername();
+        Optional<SessionToken> sessionToken = sessionTokenService.findSessionToken(username, token);
+        if (sessionToken.isEmpty()){
+            throw new IllegalArgumentException("Session token doesn't exist");
+        }
+        return new Response((short) 200, false, "Success");
     }
 }
