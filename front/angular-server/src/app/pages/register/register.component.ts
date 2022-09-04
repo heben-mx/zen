493d2ddbcbc849be0458a1ba22ee4221590f643d
Luis <luisced02031@gmail.com>
2022-09-03 19:49:14 -0500

    repaired eye icon function

--- front/angular-server/src/app/pages/register/register.component.ts
+++ front/angular-server/src/app/pages/register/register.component.ts
@@ -10,6 +10,26 @@ export class RegisterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+        const pwShowHide = document.querySelectorAll(".show-btn"),
+        pwFields = document.querySelectorAll(".password"),
+        showBtn = document.querySelector("#eye");
+
+      pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", () =>{
+          pwFields.forEach(pwField =>{
+            if((pwField as HTMLInputElement).type === "password"){
+              (pwField as HTMLInputElement).type = "text";
+              (showBtn as HTMLSpanElement).className = "bi bi-eye-fill";
+              
+            }else{
+              (pwField as HTMLInputElement).type = "password";
+              (showBtn as HTMLSpanElement).className = "bi bi-eye-slash-fill";
+            }
+          })
+
+        })
+        
+      })
   }
 
 }
