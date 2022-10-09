9975d39b1e30a456c36e97b224591b905c252213
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-10-09 16:18:51 -0300

    add: regex to validate email

--- back/src/main/java/com/heben/zen/registration/EmailValidator.java
+++ back/src/main/java/com/heben/zen/registration/EmailValidator.java
@@ -3,13 +3,17 @@ package com.heben.zen.registration;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
 
     @Override
     public boolean test(String s) {
-        // TODO: Regex to validate email
-        return true;
+        String regex = "^(.+)@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        return matcher.matches();
     }
 }
